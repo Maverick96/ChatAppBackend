456008a4f6b6970f7f03fa2e1753f61c0a184f45
Harsh Pancholi <harshpancholi8@gmail.com>
2019-09-03 18:58:41 +0530

    Added senderId to messages models

--- models/Message.js
+++ models/Message.js
@@ -14,6 +14,14 @@ module.exports = (sequelize, DataTypes) => {
                 key: 'chatId'
             }
         },
+        senderId: {
+            allowNull: false,
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'Users',
+                key: 'userId'
+            }
+        },
         message: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -22,7 +30,9 @@ module.exports = (sequelize, DataTypes) => {
     Message.associate = function (models) {
         // associations can be defined here
         const Chat = models.Chat;
+        const User = models.User;
         Chat.hasMany(Message, { foreignKey: 'chatId', sourceKey: 'chatId' });
+        User.hasMany(Message, { foreignKey: 'senderId', sourceKey: 'userId' });
     };
     return Message;
 };
